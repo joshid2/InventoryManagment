b825f6ac17ec5529a3e18ceb13b13645b06580d0
Your Name <you@example.com>
2024-11-14 13:40:59 +0000

    Added the model classes to represent the format of each CSV file - Item, Recipes and Ingredients

--- src/main/java/com/highprofitrecipes/recipe_project/model/Ingredients.java
+++ src/main/java/com/highprofitrecipes/recipe_project/model/Ingredients.java
@@ -1,4 +1,25 @@
 package com.highprofitrecipes.recipe_project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredients {
+
+    @Getter @Setter
+    private String recipeId;
+    @Getter @Setter
+    private String ingredientId;
+
+    //For ingredient Type it can be an Item or Recipe
+    @Getter @Setter
+    private String ingredientType;
+
+    //The Quantity of Items used calculated per base units
+    @Getter @Setter
+    private int quantity;
+
 }
--- src/main/java/com/highprofitrecipes/recipe_project/model/Item.java
+++ src/main/java/com/highprofitrecipes/recipe_project/model/Item.java
@@ -1,4 +1,22 @@
 package com.highprofitrecipes.recipe_project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
+
+    @Getter
+    @Setter
+    private String id;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private double cost;
+
 }
